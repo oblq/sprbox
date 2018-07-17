a6f3b7983a96615d340ea1ade06a68ad47a82097
Marco M <marcomrtr@gmail.com>
2018-07-17 16:13:16 +0200

    update documentation

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
-// Package sprbox is a toolbox factory with automatic
-// package configuration based on your build environment.
+// Package sprbox (pronunced like: SpareBox) is a toolbox factory
+// with automatic package configuration based on your build environment.
 // It provide a convenient way to handle app state
 // based on the environment.
 package sprbox
